743c9b82594406832ace294e44771d344258101a
Arsal Saeed <arsalsaeed95@gmail.com>
2026-06-30 16:12:08 +0500

    feat: add secure OTP generator and configure Gmail SMTP

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const { sendOTPEmail } = require("./src/config/emailConfig");
+const generateOTP = require("./src/utils/generateOTP");
+
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "BloodConnect API is running!" });
+});
+
+// --- TEMPORARY EMAIL TEST ROUTE ---
+app.get("/test-email", async (req, res) => {
+  try {
+    const testOTP = generateOTP();
+    // sending the test email to myself
+    await sendOTPEmail("arsalsaeed24@gmail.com", testOTP);
+    res.json({
+      success: true,
+      message: `Test email sent with OTP: ${testOTP}`,
+    });
+  } catch (error) {
+    console.error("Email Error:", error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is successfully running on port ${PORT}`);
+});
