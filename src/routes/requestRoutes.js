9bf771b38d81b7e8a3befad2fba2b3b0d7bcc00d
Mashal Areej <mashalareej92@gmail.com>
2026-07-18 18:19:27 +0500

    chore: wipe broken code, starting clean

--- src/routes/requestRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createEmergencyRequest } = require('../controllers/requestController');
-
-router.post('/create', createEmergencyRequest);
-
-module.exports = router;
